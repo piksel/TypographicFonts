622611094de114ea0befdd2fefe03e7555586e3f
nils måsén <nils@piksel.se>
2018-03-17 20:53:20 +0100

    Add TypographicFontDialog

---
+++ TypographicFonts/TypographicFontDialog.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace jnm2.TypographicFonts
+{
+    partial class TypographicFontDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("Monospace", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Normal", System.Windows.Forms.HorizontalAlignment.Left);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TypographicFontDialog));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bOk = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            listViewGroup1.Header = "Monospace";
+            listViewGroup1.Name = "lvgMono";
+            listViewGroup2.Header = "Normal";
+            listViewGroup2.Name = "lvgNormal";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1,
+            listViewGroup2});
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(222, 399);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 200;
+            // 
+            // listView2
+            // 
+            this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2});
+            this.listView2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView2.FullRowSelect = true;
+            this.listView2.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(240, 12);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(204, 311);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Width = 200;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "14",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24"});
+            this.comboBox1.Location = new System.Drawing.Point(450, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(99, 319);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "10";
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(240, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 52);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sample text";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bOk
+            // 
+            this.bOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bOk.Location = new System.Drawing.Point(450, 381);
+            this.bOk.Name = "bOk";
+            this.bOk.Size = new System.Drawing.Size(100, 30);
+            this.bOk.TabIndex = 4;
+            this.bOk.Text = "OK";
+            this.bOk.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(243, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TypographicFontDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 421);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bOk);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(575, 460);
+            this.Name = "TypographicFontDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Select Font...";
+            this.Load += new System.EventHandler(this.TypographicFontDialog_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bOk;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ TypographicFonts/TypographicFontDialog.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jnm2.TypographicFonts
+{
+    public partial class TypographicFontDialog : Form
+    {
+
+        //Dictionary<string, TypoGra fonts;
+
+        public TypographicFontDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void TypographicFontDialog_Load(object sender, EventArgs e)
+        {
+            foreach(var ff in TypographicFontFamily.InstalledFamilies)
+            {
+                //var fc = new PrivateFontCollection();
+                var normalFont = ff.NormalFont;
+                //fc.AddFontFile(normalFont.FileName);
+                var font = new FontFamily(normalFont.Name);
+                listView1.Items.Add(new ListViewItem()
+                {
+                    Text = ff.Name,
+                    Font = new Font(font, listView1.Font.Size, listView1.Font.Unit),
+                    ToolTipText = ff.Name,
+                    Tag = ff,
+                    Group = normalFont.Panose.Proportion == Panose.PanoseProportion.Monospaced 
+                        ? listView1.Groups["lvgMono"] 
+                        : listView1.Groups["lvgNormal"]
+                });
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+            if (listView1.SelectedIndices.Count < 1) return;
+
+            var fontFamily = (listView1.SelectedItems[0].Tag as TypographicFontFamily);
+            var normal = fontFamily.NormalFont;
+
+            foreach(var font in fontFamily.Fonts.OrderBy( f => f.Weight ))
+            {
+                var family = new FontFamily(font.Name);
+                var newItem = listView2.Items.Add(new ListViewItem()
+                {
+                    Text = font.SubFamily ?? "Regular" + (font.Bold?" Bold":"") + (font.Italic?" Italic":""),
+                    Font = new Font(family, listView2.Font.Size, font.FontStyle, listView2.Font.Unit),
+                    Tag = font,
+                    Selected = font == normal
+                });
+
+
+            }
+            UpdatePreview();
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdatePreview();
+        }
+
+        private void UpdatePreview()
+        {
+            if (listView2.SelectedIndices.Count < 1)
+            {
+                label1.Visible = false;
+            }
+            else
+            {
+                if (float.TryParse(comboBox1.Text, out float fontSize))
+                {
+                    label1.Visible = true;
+                    var font = (listView2.SelectedItems[0].Tag as TypographicFont);
+                    var family = new FontFamily(font.Name);
+
+                    label1.Font = new Font(family, fontSize, font.FontStyle);
+                }
+                else
+                {
+                    label1.Visible = false;
+                }
+            }
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            UpdatePreview();
+        }
+    }
+}
